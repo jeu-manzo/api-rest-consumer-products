5aa8716b99e6d8353421bc029db259b34e0ce5e0
Jeu Manzo <jeunettemanzo@gmail.com>
2019-07-09 16:37:51 -0500

    add function decodeToken

--- services/index.js
+++ services/index.js
@@ -14,4 +14,28 @@ function createToken(user) {
   return jwt.encode(payload, config.SECRET_TOKEN )
 }
 
+function decodeToken(token) {
+  const decoded = new Promise((resolve, reject) => {
+    try {
+      const payload = jwt.decode(token, config.SECRET_TOKEN)
+
+      if (payload.exp <= moment().unix()) {
+        resolve({
+          status: 401,
+          message: 'El Token ha expirado'
+        })
+      }
+
+      resolve(payload.sub)
+    } catch (err) {
+      reject({
+        status: 500,
+        message: 'Invalid Token'
+      })
+    }
+  })
+
+  return decoded
+}
+
 module.exports = createToken;
