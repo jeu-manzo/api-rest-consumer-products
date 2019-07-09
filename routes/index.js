663cc205f40a3ad547a2e441251e5bd61913c3e6
Jeu Manzo <jeunettemanzo@gmail.com>
2019-07-09 16:07:29 -0500

    add route private

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@
 const express = require('express');
 const api = express.Router();
 const productCtrlls = require('../controllers/product')
+const auth = require('../middlewares/auth');
 
 
 api.get('/product', productCtrlls.getProducts);
@@ -10,5 +11,8 @@ api.get('/product/:productId', productCtrlls.getProduct);
 api.post('/product', productCtrlls.saveProduct);
 api.put('/product/:productId', productCtrlls.updateProduct);
 api.delete('/product/:productId', productCtrlls.deleteProduct);
+api.get('/private', auth.isAuth, function(req, res) => {
+  res.status(200).send({ message: 'Tienes acceso' })
+})
 
 module.exports = api;
