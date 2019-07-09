176460316df8e77b8d98712aeb0367d5268ce596
Jeu Manzo <jeunettemanzo@gmail.com>
2019-07-09 14:01:07 -0500

    add file config.js to refactor configurations of port and database

--- index.js
+++ index.js
@@ -4,15 +4,15 @@ const mongoose = require('mongoose');
 
 const app = require('./app');
 
-const port = process.env.PORT || 3001
+const config = require('./config');
 
 
-mongoose.connect('mongodb://localhost:27017/shop', (err, res) => {
+mongoose.connect(config.db, (err, res) => {
   if (err) {
     return console.log(`Error al conectar a la base de datos ${err}`);
   }
   console.log('conexión a la base de datos estlablecida');
-  app.listen(port, () => {
-    console.log(`API REST corriendo en http:localhost:${port}`);
+  app.listen(config.port, () => {
+    console.log(`API REST corriendo en http:localhost:${config.port}`);
   })
 })
